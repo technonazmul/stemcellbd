a0efeacfb93d2d4598427c6b47c239d1484875ac
Md Nazmul Khalid <41265922+technonazmul@users.noreply.github.com>
2024-01-28 01:34:26 +0600

    update app layouts remove vite dependency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('assets/app-lhA9k1qk.css')}}">
+    <script src="assets/app-Y_MuImAH.js"></script>
+    
 </head>
 <body>
     <div id="app">
