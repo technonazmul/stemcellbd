6c4686d30315d3618b300cc7b7a941363270a2da
Md Nazmul Khalid <41265922+technonazmul@users.noreply.github.com>
2024-02-08 01:44:00 +0600

    category crud

---
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    function add_category() : Returntype {
+        
+    }
+}
