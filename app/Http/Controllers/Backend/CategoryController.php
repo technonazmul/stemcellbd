e0b822183ed6eb6a5c5d60598a5e8d8783f1dbcc
Md Nazmul Khalid <41265922+technonazmul@users.noreply.github.com>
2024-02-10 21:47:20 +0600

    category updates

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    function add_category() : Returntype {
-        
+    function add_category(Request $request) {
+        return view('backend.category.add');
     }
 }
