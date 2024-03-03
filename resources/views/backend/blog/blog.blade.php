36e7339dcc6c5ab4f6a9c9b98885124a681a70cc
M.Ripon <macbookair@MRipon.local>
2024-03-03 21:54:10 +0300

    blog

--- resources/views/backend/blog/blog.blade.php
+++ resources/views/backend/blog/blog.blade.php
@@ -1,4 +1,27 @@
 @extends('backend.dashboard')
 @section('content')
-blog list
+<div class="container">
+    <h3 class="text-center">All Blog Post</h3>
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Si.No</th>
+            <th scope="col">Title</th>
+            <th scope="col">Thumbnail</th>
+            <th scope="col">Tags</th>
+            <th scope="col">User</th>
+          </tr>
+        </thead>
+            @php $i=0 @endphp
+            @foreach($blog as $blog)
+            @php $i++@endphp
+            <td>@php echo $i @endphp </td>
+            <td></td>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+          </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
