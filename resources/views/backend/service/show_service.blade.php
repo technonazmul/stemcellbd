5377b9aa92aa4da27bd0ff2cd9edd75cbf774a77
M.Ripon <macbookair@MRipon.local>
2024-03-16 21:58:52 +0300

    service in backend

--- resources/views/backend/service/show_service.blade.php
+++ resources/views/backend/service/show_service.blade.php
@@ -1,1 +1,24 @@
-{{$show_service->name}}
+@extends('backend.dashboard')
+@section('content')
+<div class="container">
+    @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            @if(session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+@endsection
+
+
+
+{{-- @php
+                        $services=App\Models\Service::where('service_category_id',$show_services->id)->get();
+                    @endphp --}}
